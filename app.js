ab3709076ee0866fd96d8330008970cad022d5b1
andy <andy@andy-AO756.(none)>
2013-06-06 01:01:24 -0400

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+var PORT = 9000;
+var express = require("express");
+var app = express();
+var path = require('path');
+// special handling of the root folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", function(req, res){
+res.render('template.ejs', {
+layout:false
+});
+});
+// startup this server
+app.listen(PORT);
+console.log("Server on port %s", PORT);
