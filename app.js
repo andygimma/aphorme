288a0c523a6daeddfd03d702e0ac3ec8f7d03a31
andy <andy@andy-AO756.(none)>
2013-06-07 01:10:55 -0400

    fleshing out web pages

--- app.js
+++ app.js
@@ -6,9 +6,39 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res){
-res.render('template.ejs', {
-layout:false
+  res.render('template.ejs', {
+    layout:false
+  });
 });
+
+app.get("/about", function(req, res){
+  res.render('about.ejs', {
+    layout:false
+  });
+});
+
+app.get("/contact", function(req, res){
+  res.render('contact.ejs', {
+    layout:false
+  });
+});
+
+app.get("/guide", function(req, res){
+  res.render('guide.ejs', {
+    layout:false
+  });
+});
+
+app.get("/signin", function(req, res){
+  res.render('login.ejs', {
+    layout:false
+  });
+});
+
+app.get("/add", function(req, res){
+  res.render('add_term.ejs', {
+    layout:false
+  });
 });
 // startup this server
 app.listen(process.env.PORT || PORT)
